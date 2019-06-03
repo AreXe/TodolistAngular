7ff1b86ac36b547db4e31555080d86e069d58584
arexe <arexepro@gmail.com>
2019-06-03 12:19:49 +0200

    Add checked and date directives for to-do list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AddTaskComponent } from './add-task/add-task.component';
 import { TodoTaskComponent } from './todo-task/todo-task.component';
 import { DoneTaskComponent } from './done-task/done-task.component';
 import {TasksService} from './services/tasks.service';
+import { CheckedDirective } from './shared/checked.directive';
+import { DateDirective } from './shared/date.directive';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import {TasksService} from './services/tasks.service';
     AddTaskComponent,
     TodoTaskComponent,
     DoneTaskComponent,
+    CheckedDirective,
+    DateDirective,
   ],
   imports: [
     BrowserModule,
